a34bcd1fb246160bdd809b15da7e47189021a2b2
Jorge Martinez <jorgemartinezgil117@gmail.com>
2023-12-20 20:16:27 +0100

    Day 15, Task 2

--- 2023/Day15/main.go
+++ 2023/Day15/main.go
@@ -5,8 +5,48 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type Box struct {
+	labelToIdx map[string]int
+	values     []int
+}
+
+func addLens(box *Box, value int, label string) {
+	_, ok := box.labelToIdx[label]
+	if ok {
+		// Replace
+		box.values[box.labelToIdx[label]] = value
+	} else {
+		// Add
+		box.values = append(box.values, value)
+		box.labelToIdx[label] = len(box.values) - 1
+	}
+}
+
+func removeLens(box *Box, label string) {
+	idx, ok := box.labelToIdx[label]
+	if ok {
+		// Remove
+		box.values = append(box.values[:idx], box.values[idx+1:]...)
+		delete(box.labelToIdx, label)
+		for k, v := range box.labelToIdx {
+			if v > idx {
+				box.labelToIdx[k] = v - 1
+			}
+		}
+	}
+}
+
+func printBoxes(boxes []Box) {
+	for _, box := range boxes {
+		if len(box.values) != 0 {
+			fmt.Println("Box:", box)
+		}
+	}
+}
 
 func readFile(file string) []string {
 	fileHandle, err := os.Open(file)
@@ -46,11 +86,50 @@ func hashAlgorithm(s string) uint64 {
 
 func main() {
 	data := readFile("input.txt")
-	fmt.Println(data)
+	//fmt.Println(data)
 
-	var result uint64 = 0
-	for _, v := range data {
-		result += hashAlgorithm(v)
+	// Make a list of 255 boxes
+	boxes := make([]Box, 256)
+
+	// Init boxes
+	for i := 0; i < 256; i++ {
+		boxes[i] = Box{
+			labelToIdx: make(map[string]int),
+			values:     make([]int, 0),
+		}
 	}
-	fmt.Println(result)
+
+	for _, v := range data {
+		//fmt.Println("Iter:", index)
+		if strings.Contains(v, "=") {
+			// Split by "="
+			parts := strings.Split(v, "=")
+
+			//fmt.Println("Key:", parts[0], "Value:", parts[1])
+			label := parts[0]
+			id := hashAlgorithm(parts[0])
+			value, _ := strconv.Atoi(parts[1])
+
+			addLens(&boxes[id], value, label)
+		} else if strings.Contains(v, "-") {
+			// Split by "-"
+			parts := strings.Split(v, "-")
+
+			//fmt.Println("Key:", parts[0])
+
+			label := parts[0]
+			id := hashAlgorithm(parts[0])
+			removeLens(&boxes[id], label)
+		}
+		//printBoxes(boxes)
+	}
+	focusingPower := 0
+
+	for box_idx, box := range boxes {
+		for i, value := range box.values {
+			fmt.Printf("Box: %d, Lens: %d, Value: %d\n", box_idx, i, value)
+			focusingPower += (box_idx + 1) * (i + 1) * value
+		}
+	}
+	fmt.Println("Focusing power:", focusingPower)
 }
